3c589c7fd3163a5bd41e24611fd93e0946242f1d
BGGBGIBS <83280831+BGGBGIBS@users.noreply.github.com>
2023-07-07 17:51:12 +0200

    maj

--- FrontEnd/MyWayRct/src/components/footer/footer.jsx
+++
@@ -1,8 +0,0 @@
-import style from './footer.module.css'
-export default function Footer() {
-return (
-    <footer className={style['footer']}>
-        <p>&copy; 2023 MyWay by Baptiste Grosjean — All rights reserved.</p>
-    </footer>
-    );
-}
